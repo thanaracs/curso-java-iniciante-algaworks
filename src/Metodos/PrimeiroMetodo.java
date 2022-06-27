422c18ece74c2c144a9368f758677bdba8949f69
Thainara Carvalho <thai.cds1@gmail.com>
2022-06-27 14:46:24 -0300

    Primeiros metodos atualizados. enviando e recebendo parametros.

--- src/Metodos/PrimeiroMetodo.java
+++ src/Metodos/PrimeiroMetodo.java
@@ -11,52 +11,46 @@ public class PrimeiroMetodo {
 
         String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
         System.out.println("Escolha dentre os cursos abaixo: ");
-        for(int i = 0; i < cursos.length; i++) {
-            System.out.println("[" + i + "] " + cursos[i]);
-        }
+
+        iterarEExibirPosicoesDoVetorDeString(cursos);
 
         System.out.print("O curso que você deseja é o: ");
         Integer posicaoCursoEscolhido = scanner.nextInt();
         Boolean posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
 
-        if (!posicaoValida) {
-            posicaoInvalida();
-        }
+        if (!posicaoValida) posicaoInvalida();
 
         imprimirTraco();
 
         String[] formasPagamento = new String[] {"Cartão", "Boleto"};
         System.out.println("Escolha dentre as formas de pagamento abaixo: ");
 
-        for(int i = 0; i < formasPagamento.length; i++) {
-            System.out.println("[" + i + "] " + formasPagamento[i]);
-        }
+        iterarEExibirPosicoesDoVetorDeString(formasPagamento);
 
         System.out.print("Sua forma de pagamento escolhida é: ");
         Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
 
         posicaoValida = posicaoFormaPagamentoEscolhida >= 0
                 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+        if (!posicaoValida) posicaoInvalida();
 
-        if (!posicaoValida) {
-            posicaoInvalida();
-        }
 
         String cursoEscolhido = cursos[posicaoCursoEscolhido];
         String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
 
         imprimirTraco();
-
-        System.out.println("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
-
+        System.out.println("O curso escolhido foi / " + cursoEscolhido + " / e a forma de pagamento é: " + formaPagamentoEscolhida);
         scanner.close();
     }
 
+    private  static void iterarEExibirPosicoesDoVetorDeString(String[] vetor){
+        for(int i = 0; i < vetor.length; i++) {
+            System.out.println("[" + i + "] " + vetor[i]);
+        }
+    }
     static void imprimirTraco() {
         System.out.println("----------------------------------------------");
     }
-
-
 
     private static void posicaoInvalida(){
         System.err.println("Posição inválida!");
